3447990ae446eef754c52a39425b7c402e396507
배문형 <m00nlygreat@baemunhyeongs-MacBook-Air.local>
2023-05-25 00:23:06 +0900

    listview

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -5,34 +5,55 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
     print(webtoons);
-    
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        elevation: 2,
-        title: const Text(
-          '오늘의 웹툰',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          elevation: 2,
+          title: const Text(
+            '오늘의 웹툰',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
           ),
         ),
-      ),
-      body: FutureBuilder(
-        future: webtoons,
-        builder:(context, snapshot){
-          if(snapshot.hasData){
-            return const Text('There is data');
-          } 
-          return const Text('Loading...');
-        },)
-    );
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(children: [
+                const SizedBox(height:200), Expanded(child: makeList(snapshot)),
+              ],);
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Column(
+                  children: [
+                    Image.network(webtoon.thumb),
+                    Text(webtoon.title),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 40),
+            );
   }
 }
